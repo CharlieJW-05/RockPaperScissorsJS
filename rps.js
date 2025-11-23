7114420a80b4455c88dd29c4691981aa0ebcf140
Charlie Walker <charles.j.walker14@gmail.com>
2025-11-23 10:53:39 +0000

    finish round 1

---
+++ rps.js
@@ -0,0 +1,46 @@
+function computerChoice() {
+   let compChoice = Math.random();
+    if (compChoice < 0.34) {
+        return "rock";
+    } else if (compChoice <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+const computerSelection = computerChoice();
+console.log("Computer:", computerSelection); 
+
+function humanChoice() {
+    let userChoice = prompt("Do you choose rock, paper or scissors?");
+    return userChoice.toLowerCase(); 
+};
+
+const userSelection = humanChoice();
+console.log("User:", userSelection);
+
+let humanScore = 0;
+let computerScore = 0;
+
+// game logic 
+function playRound(userSelection, computerSelection) {
+    console.log(`${userSelection}' vs '${computerSelection}'`);
+
+    if (userSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (
+        (userSelection === "rock" && computerSelection === "scissors") ||
+        (userSelection === "paper" && computerSelection === "rock") ||
+        (userSelection === "scissors" && computerSelection === "paper")
+    ) {
+        humanScore++;
+        console.log("You win this round!");
+    } else {
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
+}   
+
+playRound(userSelection, computerSelection);
